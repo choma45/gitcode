0b9acc87b4c3ff3e4f6577c27b666dafe0ef6795
choma45 <phw2whp@gmail.com>
2025-06-30 14:47:43 +0900

    Add script to install Ansible

---
+++ install_ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+# ansible 설치 작업
+echo "Ansible 설치를 시작합니다."
+
+
+# epel-release 설치 
+echo "1. EPEL 저장소 설치"
+sudo dnf install -y epel-release
+
+# ansible 설치 
+echo "2. Ansible 설치"
+sudo dnf intall -y ansible 
+
+# 설치 확인 
+if command -v ansible &> /dev/null; then
+    echo "Ansible이 성공적으로 설치되었습니다."
+    ansible --version
+else 
+    echo "Ansible이 설치되지 않았습니다."
+    exit 1
+if 
